cbe116128c1518ab5c9ec7519a6d5425eac6413b
jungnamsik <nsjung74@gmail.com>
2023-03-21 14:14:14 +0900

    tab1 수정

--- src/main/java/mycompany/App2.java
+++ src/main/java/mycompany/App2.java
@@ -18,34 +18,31 @@ public class App2 implements DooraySideApp {
         result.put("layout", "B");
 
         List<Map<String, Object>> tabList = new ArrayList<>();
+            Map<String, Object> tab1 = new HashMap<>();
+            tab1.put("id", "recent1");
+            tab1.put("title", "탭1 사이드앱");
+            List<Map<String, Object>> tab1arr = new ArrayList<>();
+                Map<String, Object> tab1con = new HashMap<>();
+                tab1con.put("id", "t01");
+                tab1con.put("title", "탭1-제목");
+                tab1con.put("description", "탭1-내용");
+                tab1con.put("url", "https://ybtour.dooray.com");
 
-        Map<String, Object> tab1 = new HashMap<>();
-        tab1.put("id", "recent1");
-        tab1.put("title", "탭1 사이드앱");
-        List<Map<String, Object>> tab1arr = new ArrayList<>();
-            Map<String, Object> tab1con = new HashMap<>();
-            tab1con.put("id", "t01");
-            tab1con.put("title", "탭1-제목");
-            tab1con.put("description", "탭1-내용");
-            tab1con.put("url", "https://ybtour.dooray.com");
+                List<Map<String, String>> arr1meta = new ArrayList<>();
+                {
+                    Map<String, String> m = new HashMap<>();
+                    m.put("label", "등록자");
+                    m.put("text", "인사담당");
+                    arr1meta.add(m);
+                }
+                tab1con.put("metadata", arr1meta);
+            tab1arr.add(tab1con) ;
+            tab1.put("items", tab1arr );
 
-            List<Map<String, String>> arr1meta = new ArrayList<>();
-            {
-                Map<String, String> m = new HashMap<>();
-                m.put("label", "등록자");
-                m.put("text", "인사담당");
-                arr1meta.add(m);
-            }
-            tab1con.put("metadata", arr1meta);
-        tab1arr.add(tab1con) ;
-
-        tab1.put("items", tab1arr );
-
-        Map<String, Object> onEmpty = new HashMap<>();
-        onEmpty.put("text", "새로운 공지 사항이 없습니다.");
-        onEmpty.put("url", "https://ybtour.co.kr");
-        tab1.put("onEmpty", onEmpty);
-
+            Map<String, Object> onEmpty = new HashMap<>();
+            onEmpty.put("text", "새로운 공지 사항이 없습니다.");
+            onEmpty.put("url", "https://ybtour.co.kr");
+            tab1.put("onEmpty", onEmpty);
         tabList.add(tab1);
 
         Map<String, Object> tab2 = new HashMap<>();
